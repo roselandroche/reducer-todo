82d6803f0d95918ca8ce028c22611e2aabee6c32
roselandroche <r.landroche@gmail.com>
2019-10-18 19:00:25 -0400

    Step 1 complete

---
+++ reducer-to-do/reducers/reducer.js
@@ -0,0 +1,12 @@
+export const initialState = {
+    item: 'Default To Do',
+    completed: false,
+    id: 3892987589
+};
+
+export function reducer(state, action) {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
